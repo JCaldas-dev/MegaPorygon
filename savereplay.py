914ce26cc14724da51d5095d981a538d5081463b
JCaldas-dev <joaosantoscaldas@gmai.com>
2023-03-04 02:56:56 +0000

    login account

--- savereplay.py
+++ savereplay.py
@@ -1,36 +1,50 @@
 import websocket
 import ssl
+import json
+import time
 
+# Change these variables to your own login information
+USERNAME = "MegaPorygonBot"
+PASSWORD = "password"
+
+# URL to log in to the Pokemon Showdown server
+LOGIN_URL = "https://play.pokemonshowdown.com/action.php"
+
+# Payload for logging in
+login_payload = {
+    "act": "login",
+    "name": USERNAME,
+    "pass": PASSWORD,
+    "challstr": "",
+}
+
+# Event called when the WebSocket connection is opened
+def on_open(ws):
+    print("WebSocket connected.")
+    # Send the login payload
+    ws.send(json.dumps(login_payload))
+
+# Event called when a message is received through the WebSocket connection
 def on_message(ws, message):
     print(message)
 
+# Event called when an error occurs in the WebSocket connection
 def on_error(ws, error):
     print(error)
 
+# Event called when the WebSocket connection is closed
 def on_close(ws):
-    print("### closed ###")
+    print("WebSocket disconnected.")
 
-def on_open(ws):
-    def run(*args):
-        ws.send("{'event':'challstr'}")
-        ws.send("{'event':'chat','data':'/join global'}")
-        ws.send("{'event':'chat','data':'/nick PokemonBot'}")
-        ws.send("{'event':'chat','data':'!code jibxjhdw'}")
-        ws.send("{'event':'chat','data':'/join battle-gen8randombattle-1234567890'}")
-        ws.send("{'event':'chat','data':'!pick random'}")
-        ws.send("{'event':'chat','data':'Hi, I am a bot!'}")
-        ws.send("{'event':'chat','data':'Good luck and have fun!'}")
-    thread.start_new_thread(run, ())
+# Connect to the Pokemon Showdown WebSocket server
+ws_url = "wss://sim.smogon.com/showdown/websocket"
+ws = websocket.WebSocketApp(
+    ws_url,
+    on_message=on_message,
+    on_error=on_error,
+    on_close=on_close,
+    on_open=on_open,
+)
 
-if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws_url = "wss://sim.smogon.com/showdown/websocket"
-    ws = websocket.WebSocketApp(
-        ws_url,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close,
-        on_open=on_open,
-        header={'User-Agent': 'Mozilla/5.0'}
-    )
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+# Start the WebSocket connection
+ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
